9de85dbdca2d444d36e8694adef339382a661fa8
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2022-01-05 19:37:41 +0300

    Removing unused imports

--- src/main/java/com/example/AnimalRecording/Model/Animal.java
+++ src/main/java/com/example/AnimalRecording/Model/Animal.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.objenesis.instantiator.basic.NewInstanceInstantiator;
-import org.springframework.stereotype.Component;
 
-import javax.annotation.sql.DataSourceDefinition;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
