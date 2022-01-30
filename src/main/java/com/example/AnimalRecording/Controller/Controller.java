256301210918efe4a2f25d8047f2672d268d3419
AbdulkadirKilic <kilicabdulkadir32@gmail.com>
2022-01-30 22:35:07 +0300

    Post Method Update for Status 201

--- src/main/java/com/example/AnimalRecording/Controller/Controller.java
+++ src/main/java/com/example/AnimalRecording/Controller/Controller.java
@@ -3,7 +3,11 @@ package com.example.AnimalRecording.Controller;
 import com.example.AnimalRecording.Model.Animal;
 import com.example.AnimalRecording.Service.AnimalService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,42 +19,41 @@ public class Controller {
 
 
     @GetMapping()
-    public List<Animal> getAnimals(){
+    public List<Animal> getAnimals() {
 
         return animalService.getAnimals();
 
     }
 
     @GetMapping("/{id}")
-    public Animal getAnimal1(@PathVariable ("id") String animalId){
+    public Animal getAnimal1(@PathVariable("id") String animalId) {
 
         return animalService.getAnimalById(animalId);
 
     }
 
     @PostMapping
-    public Animal post(@RequestBody  Animal newAnimal){
+    public ResponseEntity<Animal> post(@RequestBody Animal newAnimal) {
+        Animal savedAnimal = animalService.create(newAnimal);
 
-        return  animalService.create(newAnimal);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedAnimal.getId()).toUri();
 
+        return ResponseEntity.created(location).build();
     }
 
     @PutMapping("/{id}")
-    public void update(@PathVariable("id") String animalId, @RequestBody Animal newAnimal ){
+    public void update (@PathVariable("id") String animalId, @RequestBody Animal newAnimal ){
 
-         animalService.put(newAnimal,animalId);
+        animalService.put(newAnimal, animalId);
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") String id){
+    public void delete (@PathVariable("id") String id){
 
         animalService.delete(id);
 
     }
 
 
-
-
-
-
 }
+
